57af11e98090d0be655e6c605edc17dd81ea97a6
Elanchez <elan.r47@gmail.com>
2021-01-27 12:16:45 +0530

    now host it

--- Resume/settings.py
+++ Resume/settings.py
@@ -128,4 +128,4 @@ STATICFILES_DIRS = [  os.path.join(BASE_DIR,'static') ]
 STATIC_ROOT= os.path.join(BASE_DIR,'assets/')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
